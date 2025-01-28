7de878dde3e011f345dc0f935fbb520e35549b87
mattm3192 <mattm3192@gail.com>
2025-01-28 14:15:10 -0600

    added circleshape class, and created player.py file

--- main.py
+++ main.py
@@ -9,13 +9,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type ==pygame.QUIT:
                 return
-        pygame.surface.Surface
-        pygame.Surface.fill(screen, (0, 0, 0))
+            
+        screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
     
 if __name__ == "__main__":
     main()
