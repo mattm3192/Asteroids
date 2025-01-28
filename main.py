1f78993c06c6fe284fea2e3bb4eafef3dc91bae7
mattm3192 <mattm3192@gail.com>
2025-01-28 14:31:41 -0600

    added player class and updated main to draw it to screen

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
+
 def main():
     pygame.init
     print("Starting asteroids!")
@@ -11,13 +14,15 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type ==pygame.QUIT:
                 return
             
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
