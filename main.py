79e960f65d2edd03e5802d04867cc28f8ea5f1cc
mattm3192 <mattm3192@gail.com>
2025-01-29 09:36:15 -0600

    added asteroid vields and collision with player

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init
@@ -13,7 +15,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+   
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
 
     while True:
         for event in pygame.event.get():
@@ -21,8 +34,14 @@ def main():
                 return
             
         screen.fill("black")
-        player1.update(dt)
-        player1.draw(screen)
+        for thing in updatable:
+            thing.update(dt)
+            for asteroid in asteroids:
+                if asteroid.collision(player):
+                    print("Game over!")
+                    exit()
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
--- player.py
+++ player.py
@@ -17,8 +17,7 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
-        #return super().draw(screen)
-    
+            
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
